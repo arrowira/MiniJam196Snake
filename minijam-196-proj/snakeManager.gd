09c7cac420910538990422ed77201e8b30dc558a
arrowira <41136800+arrowira@users.noreply.github.com>
2025-10-24 21:03:11 -0700

    fixedSnakeMovement

--- minijam-196-proj/snakeManager.gd
+++ minijam-196-proj/snakeManager.gd
@@ -1,20 +1,25 @@
 extends Node2D
-var segments = 5
+var segments = 20
 var snakeSegments = []
-var snakeSegmentObjects
 var player1segment := preload("res://player_1_segment.tscn")
 func _ready() -> void:
 	for i in range(segments):
 		var newSegment := player1segment.instantiate()
-		newSegment.position = Vector2(0, 0)
+		var newSegmentPos := Vector2(-i*32,0)
+		newSegment.position = newSegmentPos
 		$segmentContainer.add_child(newSegment)
-		snakeSegments.append(Vector2(0,0))
+		snakeSegments.append(newSegmentPos)
+
 
 
 
 func updateSnake(headPosition: Vector2):
+	var newSnakeSegments = snakeSegments.duplicate(true)
 	for i in range(len(snakeSegments)-1):
-		snakeSegments[i+1]=snakeSegments[i]
-	snakeSegments[0] = headPosition
+		newSnakeSegments[i+1]=snakeSegments[i]
+	
+	newSnakeSegments[0] = headPosition
 	for i in range(len($segmentContainer.get_children())):
-		$segmentContainer.get_children()[i].position = snakeSegments[i]
+		$segmentContainer.get_children()[i].position = newSnakeSegments[i]
+	
+	snakeSegments = newSnakeSegments.duplicate(true)
