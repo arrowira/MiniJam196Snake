e7027388abed169ba7af74af67d73699ad1aced1
arrowira <41136800+arrowira@users.noreply.github.com>
2025-10-24 21:58:20 -0700

    turn wait times

--- minijam-196-proj/scripts/player1Movement.gd
+++ minijam-196-proj/scripts/player1Movement.gd
@@ -7,23 +7,30 @@ var turn = 0
 var turnLimit = 10
 var stepTime = 3
 var running = true
+
+var turnStarted = false
+
 func _ready() -> void:
 	position = Vector2(0,0)
 func _physics_process(delta: float) -> void:
 	#inputs
 	if Input.is_action_just_pressed("p1down") and direction!=Vector2(0,-1):
 		direction = Vector2(0,1)
+		turnStarted = true
 	if Input.is_action_just_pressed("p1up") and direction!=Vector2(0,1):
 		direction = Vector2(0,-1)
+		turnStarted = true
 	if Input.is_action_just_pressed("p1right") and direction!=Vector2(-1,0):
 		direction = Vector2(1,0)
+		turnStarted = true
 	if Input.is_action_just_pressed("p1left") and direction!=Vector2(1,0):
 		direction = Vector2(-1,0)
+		turnStarted = true
 
 	
 	
 	timeSinceLastMove += delta*speed
-	if timeSinceLastMove > stepTime and running:
+	if timeSinceLastMove > stepTime and running and turnStarted:
 		turn+=1
 		position = position + direction*32
 		timeSinceLastMove = 0
@@ -31,4 +38,5 @@ func _physics_process(delta: float) -> void:
 	if turn>turnLimit:
 		turn = 0
 		running = false
+		turnStarted=false
 		get_parent().get_parent().get_node("snakeB").get_node("head").running=true
