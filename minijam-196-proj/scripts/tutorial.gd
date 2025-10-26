53307b1f2b83becc417db07fbe14e2075745c169
arrowira <41136800+arrowira@users.noreply.github.com>
2025-10-26 13:29:26 -0700

    part2 tutorial

--- minijam-196-proj/scripts/tutorial.gd
+++ minijam-196-proj/scripts/tutorial.gd
@@ -1,10 +1,13 @@
 extends Node2D
+var part = 1
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("p1down") or Input.is_action_just_pressed("p1up") or Input.is_action_just_pressed("p1left") or Input.is_action_just_pressed("p1right"):
+	if part ==1 and (Input.is_action_just_pressed("p1down") or Input.is_action_just_pressed("p1up") or Input.is_action_just_pressed("p1left") or Input.is_action_just_pressed("p1right")):
 		$pt1.visible = false
 		$pt1Timer.start()
-	
+	elif part == 2 and (Input.is_action_just_pressed("p2down") or Input.is_action_just_pressed("p2up") or Input.is_action_just_pressed("p2left") or Input.is_action_just_pressed("p2right")):
+		$pt2.visible = false
 
 
 func _on_pt_1_timer_timeout() -> void:
-	pass # Replace with function body.
+	$pt2.visible = true
+	part = 2
