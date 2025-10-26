4ce1f66e77e9545c3817172f1288e4c4c35f5592
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-10-25 20:17:08 -0700

    TurnCount

--- minijam-196-proj/scenes/main.gd
+++ minijam-196-proj/scenes/main.gd
@@ -9,3 +9,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		txt.text = "[color=blue]Blue[/color]"
 		
+	var TimeLeft1 = get_node("/root/main/Hud/Rem/Blue")
+	var TimeLeft2 = get_node("/root/main/Hud/Ram/Red")
+	var WhosRam = get_tree().current_scene.get_node("snakeA/head")
+	var WhosRem = get_tree().current_scene.get_node("snakeB/head")
+	TimeLeft1.bbcode_enabled = true
+	TimeLeft2.bbcode_enabled = true
+	if(whosTurn == 2):
+		TimeLeft2.text = "[color=red]" + str(WhosRam.turnLimit - WhosRam.turn) + "[/color]"
+		TimeLeft1.text = "[color=blue]0[/color]"
+	else:
+		TimeLeft1.text = "[color=blue]" + str(WhosRem.turnLimit - WhosRem.turn) + "[/color]"
+		TimeLeft2.text = "[color=red]0[/color]"
