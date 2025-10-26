f76444b14d76bb320e3b87073f7ffca7bc5f6091
arrowira <41136800+arrowira@users.noreply.github.com>
2025-10-26 13:20:49 -0700

    part1 of tutorial

--- minijam-196-proj/scripts/tutorial.gd
+++ minijam-196-proj/scripts/tutorial.gd
@@ -1,1 +1,10 @@
 extends Node2D
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("p1down") or Input.is_action_just_pressed("p1up") or Input.is_action_just_pressed("p1left") or Input.is_action_just_pressed("p1right"):
+		$pt1.visible = false
+		$pt1Timer.start()
+	
+
+
+func _on_pt_1_timer_timeout() -> void:
+	pass # Replace with function body.
