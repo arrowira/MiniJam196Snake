d630c8466e5b5a326e56ebe7c0329032fc09537b
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-10-25 19:37:42 -0700

    Turns

--- minijam-196-proj/scenes/main.gd
+++ minijam-196-proj/scenes/main.gd
@@ -1,2 +1,11 @@
 extends Node2D
-var whosTurn = 1
+var whosTurn = 2 #Who's Rem
+
+func _physics_process(delta: float) -> void:
+	var txt = get_node("/root/main/Hud/RemOrRam")
+	txt.bbcode_enabled = true
+	if(whosTurn == 2):
+		txt.text = "[color=red]Red[/color]"
+	else:
+		txt.text = "[color=blue]Blue[/color]"
+		
