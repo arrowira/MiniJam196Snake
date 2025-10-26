9de5d986202bc59f723ce1bf0ceb739c2f8b0d4f
arrowira <41136800+arrowira@users.noreply.github.com>
2025-10-26 13:32:39 -0700

    pt3 of tutorial

--- minijam-196-proj/scripts/tutorial.gd
+++ minijam-196-proj/scripts/tutorial.gd
@@ -6,8 +6,19 @@ func _process(delta: float) -> void:
 		$pt1Timer.start()
 	elif part == 2 and (Input.is_action_just_pressed("p2down") or Input.is_action_just_pressed("p2up") or Input.is_action_just_pressed("p2left") or Input.is_action_just_pressed("p2right")):
 		$pt2.visible = false
+		$pt2Timer.start()
 
 
 func _on_pt_1_timer_timeout() -> void:
 	$pt2.visible = true
 	part = 2
+
+
+func _on_pt_2_timer_timeout() -> void:
+	$pt3.visible = true
+	$pt3Timer.start()
+	part = 3
+
+
+func _on_pt_3_timer_timeout() -> void:
+	$pt3.visible = false
